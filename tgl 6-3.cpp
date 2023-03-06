c3a0cf8b894f302928291685b91710359fbe8e01
SafinatunNaja29 <safinatun.nj29@gmail.com>
2023-03-06 15:49:09 +0700

    first commit

---
+++ tgl 6-3.cpp
@@ -0,0 +1,23 @@
+//Library
+#include <iostream>
+using namespace std; 
+
+//program main
+int main(){//begin
+
+    //numeric p,L,luas
+    double p,l,luas;
+    //display "masukan panjang"
+    cout<<"masukan panjang = ";
+    //accept p
+    cin>>p;
+    //display "masukan lebar"
+    cout<<"masukan lebar = ";
+    //accept L
+    cin>>l;
+    //compute Luas=p*L
+    luas=p*l;
+    //display "Luas persegi panjang adalah",Luas
+    cout<<"luas persegi panjang adalah "<<luas;
+
+}//end
